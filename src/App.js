b829b15d055c3bd1d7bdd1e4b49ded518a0336d9
unknown <ahmad.tansheet@gmail.com>
2021-09-22 10:50:44 +0500

    2nd last class

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React from "react";
+import Register from "./components/Register";
+import Login from "./components/Login";
+import './App.css'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+
+const App = () => {
+  return (
+      <>
+      <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/register">Register</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Switch>
+      <Route path="/" exact>
+          <h1>Home Component</h1>
+        </Route>
+      
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/register">
+          <Register />
+        </Route>
+      </Switch>
+      
+      </Router>
+
+      </>
+  );
+}
+
+export default App
